3364af3acea2e3eb818100578b6abc5341d7a014
ishitadkothari <ishitadkothari@gmail.com>
2018-03-22 15:48:14 +0530

    package changes

--- src/SimpleForm.js
+++ src/SimpleForm.js
@@ -12,11 +12,11 @@ const SimpleForm = ({ status, message, className, style, onSubmitted }) => {
 
   return (
     <div className={className} style={style}>
-      {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
-      {status === "error" && <div style={{ color: "red" }}>{message}</div>}
-      {status === "success" && <div style={{ color: "green" }}>{message}</div>}
       <input ref={node => (input = node)} type="email" placeholder="Your email" />
       <button onClick={submit}>Submit</button>
+      {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
+      {status === "error" && <div style={{ color: "red" }}>You are already Subscribed</div>}
+      {status === "success" && <div style={{ color: "green" }}>{message}</div>}
     </div>
   );
 };
